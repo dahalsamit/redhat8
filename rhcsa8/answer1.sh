7f3d3815af417270c510d2710b562aaf85182ae3
dahalsamit <dahalsamit@github.com>
2019-12-26 21:33:59 +1100

    rhce8 objectives module 1

--- rhcsa8/answer1.sh
+++ rhcsa8/answer1.sh
@@ -20,7 +20,26 @@ ls -al /proc/1/fd/* 2> /root/stderr.out
 echo  "Q6 redirect stdout to /dev/null"
 ls -al /proc 1 > /dev/null
 
-echo "Q7"
+echo "Q7 listing ip address from access.log"
+grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" access.log 
 
-echo "Q8"
+echo "Q7A listing unique address count sorted" 
+grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" access.log | uniq -c |sort 
+
+echo "Q8 find email address from the regex.txt file"
+grep '[a-z+._%0-9-]\+@[a-z]\+\.[a-z]\+' regex.txt  
+
+echo "Q8 find fqdn from regex.txt"
+grep '[a-zA-Z0-9+._-%]\+\.\+[a-z]\+\.[a-z]\+' regex.txt 
+
+echo "Q8 find ip address"
+grep -o -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' regex.txt
+
+echo "Q8 find line with Mr."
+grep '^Mr\.' regex.txt 
+
+echo "Q8 word ending with ing"
+grep -o '\s*\S\+ing' regex.txt 
+ 
+
 
